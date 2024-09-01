b72a8b95383e40c42d5d9bc48650f372bad24ead
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-09-01 14:12:44 +0200

    Dynamic menu fetching establishments

--- app/e/[id]/page.tsx
+++ app/e/[id]/page.tsx
@@ -8,22 +8,22 @@ import { Button } from '@/components/ui/button';
 import api from '@/app/api/api';
 
 interface Product {
-  id: string;
+  id: number;
   name: string;
   description: string;
-  currentPrice: number;
+  price: number;
   availability: boolean;
-  categoryId: string;
+  category: { id: number};
 }
 
 interface ProductCategory {
-  id: string;
+  id: number;
   name: string;
   description?: string;
 }
 
 interface Establishment {
-  id: string;
+  id: number;
   name: string;
   address: string;
   phone: string;
@@ -35,28 +35,14 @@ interface Establishment {
   description: string;
 }
 
-// Datos de ejemplo para categorías y productos
-const productCategories: ProductCategory[] = [
-  { id: '1', name: 'Entrantes' },
-  { id: '2', name: 'Platos principales' },
-  { id: '3', name: 'Postres' },
-  { id: '4', name: 'Bebidas' },
-];
-
-const products: Product[] = [
-  { id: '1', name: 'Ensalada César', description: 'Lechuga, pollo, parmesano y aderezo César', currentPrice: 7.50, availability: true, categoryId: '1' },
-  { id: '2', name: 'Pizza Margarita', description: 'Tomate, mozzarella y albahaca', currentPrice: 10.00, availability: true, categoryId: '2' },
-  { id: '3', name: 'Brownie de chocolate', description: 'Brownie con helado de vainilla', currentPrice: 5.00, availability: false, categoryId: '3' },
-  { id: '4', name: 'Jugo de Naranja', description: 'Jugo de naranja natural', currentPrice: 3.00, availability: true, categoryId: '4' },
-];
-
 const EstablishmentPage: React.FC = () => {
   const { id } = useParams();
-  const [selectedCategory, setSelectedCategory] = useState<string>(productCategories[0].id);
+  const [selectedCategory, setSelectedCategory] = useState<number>();
   const [establishment, setEstablishment] = useState<Establishment | null>(null);
-
+  const [productCategories, setProductCategories] = useState<ProductCategory[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  
   useEffect(() => {
-    // Fetch the establishment data from the backend
     const fetchEstablishmentData = async () => {
       try {
         const response = await api.get(`/establishments/${id}`);
@@ -66,25 +52,48 @@ const EstablishmentPage: React.FC = () => {
       }
     };
 
+    const fetchProductCategories = async () => {
+      try {
+        const response = await api.get(`/product-categories/${id}`);
+        setProductCategories(response.data);
+        setSelectedCategory(response.data[0]?.id || '');
+      } catch (error) {
+        console.error('Error fetching product categories:', error);
+      }
+    };
+
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get(`/products/establishment/${id}`);
+        setProducts(response.data);
+        
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
     fetchEstablishmentData();
+    fetchProductCategories();
+    fetchProducts();
   }, [id]);
 
-  const handleCategoryClick = (categoryId: string) => {
+  const handleCategoryClick = (categoryId: number) => {
     setSelectedCategory(categoryId);
   };
 
-  const filteredProducts = products.filter(product => product.categoryId === selectedCategory);
-
   const handleAddProduct = () => {
-    window.location.href += '/add-product';
+    window.location.href = `/e/${id}/add-product`;
   };
+  const filteredProducts = products.filter(product => product.category.id === selectedCategory);
+  
 
   return (
     <div className="container mx-auto py-8">
-      <div className='flex w-full justify-between'>
-        <BackButton></BackButton>
+      <div className="flex w-full justify-between">
+        <BackButton />
         <Button onClick={handleAddProduct}>+ Añadir producto</Button>
       </div>
+      
       {establishment && (
         <div className="mb-8">
           <h1 className="text-4xl font-bold mb-4">{establishment.name}</h1>
@@ -92,7 +101,7 @@ const EstablishmentPage: React.FC = () => {
           <p><strong>Teléfono:</strong> {establishment.phone}</p>
           <p><strong>Tipo:</strong> {establishment.type}</p>
           <p><strong>Capacidad:</strong> {establishment.capacity}</p>
-          <p><strong>Horario de operación:</strong> {establishment.operatingHours}</p>
+          <p><strong>Horario:</strong> {establishment.operatingHours}</p>
           <p><strong>Email:</strong> {establishment.email}</p>
           <p><strong>Sitio web:</strong> <a href={establishment.website} target="_blank" rel="noopener noreferrer">{establishment.website}</a></p>
           <p><strong>Descripción:</strong> {establishment.description}</p>
@@ -117,7 +126,7 @@ const EstablishmentPage: React.FC = () => {
             key={product.id}
             name={product.name}
             description={product.description}
-            currentPrice={product.currentPrice}
+            price={product.price}
             availability={product.availability}
           />
         ))}
