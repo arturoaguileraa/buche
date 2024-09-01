48b2b74dd28f5c1c24a43834f53d0dbdb5522cf3
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-09-01 14:12:11 +0200

    SessionButton deleted on select-rol page.

--- app/select-role/page.tsx
+++ app/select-role/page.tsx
@@ -43,7 +43,6 @@ const SelectRolePage = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <SessionButton></SessionButton>
       <div className="bg-white p-8 rounded shadow-md max-w-md w-full text-center">
         <h1 className="text-3xl font-bold mb-4">Bienvenido a Buche</h1>
         <p className="mb-6 text-gray-700">
