60d396ce5f239b717c2f6aaeda1828fb9cff7255
arturoaguileraa <arturoaguileraglez@gmail.com>
2024-09-10 00:13:33 +0200

    Meta tags added

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,6 +6,7 @@ import { useProfileData } from '../utils/jwtUtils';
 import BarCard from '@/components/ui/barcard';
 import { Button } from '@/components/ui/button';
 import Loader from '@/components/ui/loader';
+import Head from 'next/head';
 
 type Bar = {
     id: string;
@@ -190,12 +191,31 @@ const Home = () => {
     };
 
     return (
-        <div>
-            {profileData?.roles === 'CLIENT' && renderClientView()}
-            {profileData?.roles === 'OWNER' && renderOwnerView()}
-            {profileData?.roles === 'WAITER' && renderWaiterView()}
-            {profileData?.roles === 'ADMIN' && renderAdminView()}
-        </div>
+        <>
+            <Head>
+            {/* Meta tags para SEO y redes sociales */}
+            <title>buche - Tu app de gestión de pedidos</title>
+            <meta name="description" content="Gestiona fácilmente tus pedidos con Buche, la mejor herramienta para establecimientos." />
+
+            {/* Open Graph meta tags */}
+            <meta property="og:title" content="Buche - Tu app de gestión de pedidos" />
+            <meta property="og:description" content="Gestiona fácilmente tus pedidos con Buche, la mejor herramienta para establecimientos." />
+            <meta property="og:image" content="https://buche-frontend.vercel.app/buche-template.png"/>
+            <meta property="og:url" content="https://buche-frontend.vercel.app" />
+
+            {/* Twitter Card meta tags */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="Buche - Tu app de gestión de pedidos" />
+            <meta name="twitter:description" content="Gestiona fácilmente tus pedidos con Buche, la mejor herramienta para establecimientos." />
+            <meta name="twitter:image" content="https://buche-frontend.vercel.app/buche-template.png"/>
+        </Head>
+            <div>
+                {profileData?.roles === 'CLIENT' && renderClientView()}
+                {profileData?.roles === 'OWNER' && renderOwnerView()}
+                {profileData?.roles === 'WAITER' && renderWaiterView()}
+                {profileData?.roles === 'ADMIN' && renderAdminView()}
+            </div>
+        </>
     );
 };
 
